bad600e4ff18da941ac4e3158b20bded59bc48d6
Igor Martins <figortmartins@gmail.com>
2020-10-05 15:16:33 -0300

    Update calc
    Percent created

--- calc.js
+++ calc.js
@@ -15,9 +15,14 @@ const sub = (n1, n2) => {
     return n1 - n2
 }
 
+const percent = (n1, percent) => {
+    return n1 / (percent / 100)
+}
+
 module.exports = {
     soma,
     mult,
     div,
-    sub
+    sub,
+    percent
 }
